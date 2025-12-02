374a2ad7b395eec8225c7d150badd6f7d6d4a5ee
Gustavo <glinares@unsa.edu.pe>
2025-12-02 18:10:16 -0500

    Avance de la página peliculas.html , detalles.html

---
+++ static/js/peliculas.js
@@ -0,0 +1,24 @@
+fetch("../../peliculas.json")
+    .then(response => response.json())
+    .then(data => mostrarPeliculas(data));
+
+function mostrarPeliculas(lista) {
+    const contenedor = document.getElementById("lista-peliculas");
+
+    lista.forEach(peli => {
+        const tarjeta = document.createElement("div");
+        tarjeta.classList.add("card-pelicula");
+
+        tarjeta.innerHTML = `
+            <img src="../img/${peli.Imagen}" alt="${peli.Título}">
+            <h3>${peli.Título}</h3>
+            <button onclick="verDetalle(${peli.ID})">Ver más</button>
+        `;
+
+        contenedor.appendChild(tarjeta);
+    });
+}
+
+function verDetalle(id) {
+    window.location.href = `detalle.html?id=${id}`;
+}
